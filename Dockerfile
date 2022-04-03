c20c0b0aa280aa52cce6e1f676716b80b8fce71b
GoodOlClint <goodolclint@gmail.com>
2022-04-02 23:49:51 -0500

    add NODES option

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN apt update \
     && mv dogecoin-1.14.5 /root/dogecoin-bin \
     && rm dogecoin.tar.gz
 EXPOSE 22556
+ENV NODES null
 ENV PATH $PATH:~/dogecoin-bin/bin/
 ENTRYPOINT ["/bin/bash"]
 CMD ["/init.sh"]
--- init.sh
+++ init.sh
@@ -10,6 +10,7 @@ if [ ! -f ~/.dogecoin/dogecoin.conf ]; then
     PWord=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1`
     echo rpcpassword=$PWord >> ~/.dogecoin/dogecoin.conf
 fi
+
 ##Remove bootstrap.dat.old if it exists
 if [ -f ~/.dogecoin/bootstrap.dat.old ]; then
     rm ~/.dogecoin/bootstrap.dat.old
@@ -17,4 +18,18 @@ fi
 
 ##Start dogecoind daemon
 echo Running dogecoin
-~/dogecoin-bin/bin/dogecoind -maxconnections=500 -printtoconsole -shrinkdebugfile
+
+~/dogecoin-bin/bin/dogecoind -maxconnections=500 -printtoconsole -shrinkdebugfile &
+
+if [ "$NODES" != "null" ]; then
+    echo "NODES $NODES"
+    IFS=' ' read -ra dogenodes <<< $NODES
+    for i in "${dogenodes[@]}"
+    do
+        echo "Adding Node $i"
+        sleep 15
+        ~/dogecoin-bin/bin/dogecoin-cli addnode $i add
+    done
+fi
+
+wait -n
